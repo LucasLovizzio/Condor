5dfcb477bde16c65686d7f598c92068fb7c1eb79
Lucas <llovizzio@gmail.com>
2025-07-17 20:01:29 -0300

    eliminado metodo repetido de MailOrder

--- src/com/condor/technologies/mails/MailOrderService.java
+++ src/com/condor/technologies/mails/MailOrderService.java
@@ -13,26 +13,11 @@ public class MailOrderService {
 
         System.out.println("\n=== CRITERIO DE ORDENAMIENTO ===");
         System.out.println(ordenamiento);
-        explicarCriterios(ordenamiento);
 
         System.out.println("\n=== MAILS ORDENADOS ===");
         imprimirMails(mailsOrdenados);
 
         return mailsOrdenados;
-    }
-
-    private void explicarCriterios(String ordenamiento) {
-        String[] criterios = ordenamiento.split("\\|");
-
-        for (int i = 0; i < criterios.length; i++) {
-            String criterio = criterios[i].trim();
-            if (criterio.matches("^(!?)([A-Z]+)-(FIFO|LIFO)$")) {
-                String[] partes = criterio.split("-");
-                boolean esNegacion = partes[0].startsWith("!");
-                String flag = esNegacion ? partes[0].substring(1) : partes[0];
-                String orden = partes[1];
-            }
-        }
     }
 
     private void imprimirMails(List<Mail> mails) {
